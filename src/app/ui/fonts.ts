db2f772f70295dffb112d8aa4304a42209b119d3
Andrey Roditelev <rodandr13@gmail.com>
2023-11-02 13:19:01 +0300

    feat: add fonts and del tamplate text

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Roboto_Condensed } from 'next/font/google';
+
+export const robotoCondensed = Roboto_Condensed({
+  subsets: ['cyrillic'],
+  display: 'swap',
+  weight: '400',
+  variable: '--roboto-condensed',
+});
